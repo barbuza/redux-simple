842d7cd4b8cb317dc774db6c84d4a69bfd7af17f
Victor Kotseruba <barbuzaster@gmail.com>
2016-07-15 17:24:37 -0700

    deps update, react 15

---
+++ tests/stress-test.js
@@ -0,0 +1,63 @@
+import test  from './base';
+import React from 'react';
+import PureComponent from 'react-pure-render/component';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import { createStore } from 'redux'
+
+import { Provider, connect } from '../src';
+
+function items(state = { loading: false }, action) {
+  switch (action.type) {
+  case 'START':
+    return { loading: true };
+  case 'STOP':
+    return { loading: false };
+  default:
+    return state;
+  }
+}
+
+@connect(state => state)
+class Indicator extends PureComponent {
+
+  render() {
+    const { loading } = this.props;
+    return (
+      <div>{loading ? 1 : 0}</div>
+    );
+  }
+
+}
+
+test('stress', t => {
+  const store = createStore(items);
+
+  const res = TestUtils.renderIntoDocument(
+    <Provider store={store}>
+      <Indicator />
+    </Provider>
+  );
+
+  t.plan(1);
+
+  const timeouts = [];
+
+  for (let i = 0; i < 1000; i++) {
+    timeouts.push(i);
+  }
+
+  timeouts.forEach(timeout => {
+    setTimeout(() => {
+      store.dispatch({
+        type: (timeout % 2) ? 'STOP' : 'START'
+      });
+    }, timeout);
+  });
+
+  setTimeout(() => {
+    const resNode = ReactDOM.findDOMNode(res);
+    t.equal(resNode.textContent, '0');
+  }, 1100);
+
+});
